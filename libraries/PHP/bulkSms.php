74952ace6e22e656d14ec7b2d32617b51eeb0694
Jarosław Gil <zafahix@gmail.com>
2018-06-25 12:07:25 +0200

    + curl option

--- libraries/PHP/bulkSms.php
+++ libraries/PHP/bulkSms.php
@@ -105,6 +105,7 @@ class bulkSms
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		$result = curl_exec($ch);
 		$return['response'] = json_decode($result, true);
 		$return['code'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
